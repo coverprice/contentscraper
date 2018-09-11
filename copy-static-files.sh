d4899e5dedfcd1323b30d239077bf57e9daffbc1
International Hobo <2594816+coverprice@users.noreply.github.com>
2018-09-10 23:43:06 -0700

    Create static dir if it doesn't exist

--- copy-static-files.sh
+++ copy-static-files.sh
@@ -4,4 +4,7 @@ SCRIPT_DIR=$(dirname $(readlink -f $0))
 if [[ -z ${GOPATH} ]]; then
   echo "GOPATH not defined" >&2
 fi
+if [[ ! -d "${SCRIPT_DIR}/server/static" ]]; then
+  mkdir -p "${SCRIPT_DIR}/server/static"
+fi
 cp "${SCRIPT_DIR}/server/static/"* "${GOPATH}/bin/static/"
